e32c06c107ebcc7b08a38850b86446e805349e29
Aakansha <monstershome@gmail.com>
2015-04-19 18:00:56 +0530

    Initial checkin of Sample Project

---
+++ SampleProject/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("\n");//new line
+		System.out.print("Computer\tApplications\n");//tab and new line
+		System.out.println("\u0910\t\u0911");//unicode for hindi alphabet
+		System.out.println("\"hello I am learning Java\"");//double quotes
+
+	}
+
+}
